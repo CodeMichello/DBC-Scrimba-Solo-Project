53e31a8f35c96ce795128dd5896ebd4835d3772d
Michello <michaeladjah.geek@gmail.com>
2023-02-27 17:42:52 +0000

    Initial Commit

---
+++ components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Info() {
+    return (
+        <div>
+            <img src="../Assets/profile.png" alt="Profile Picture" />
+            <h2>Laura Smith</h2>
+            <p>Frontend Developer</p>
+            <span>laurasmith.website</span>
+        </div>
+    )
+}
